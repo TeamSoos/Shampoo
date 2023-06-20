b92dd559ff0da65ed7aba08eb0ede1cdebeaedc0
thepiguy <kunal.a.dandekar@gmail.com>
2023-06-20 14:21:44 +0200

    Made load and unload events

--- GUI/ViewModels/MainWindowViewModel.cs
+++ GUI/ViewModels/MainWindowViewModel.cs
@@ -34,12 +34,14 @@ public class MainWindowViewModel : ReactiveObject, IHostScreen {
     public RoutingState Router { get; } = new RoutingState();
 
     public void GoNext(RoutablePage page) {
+        stack.GetTopPage().OnUnload();
         stack.GoTo(page);
         Router.Navigate.Execute(page);
     }
 
     public void GoBack() {
         GoBackPage.Execute();
+        stack.GetTopPage().OnLoad();
     }
 
     public bool mobileUI { get; set; }
