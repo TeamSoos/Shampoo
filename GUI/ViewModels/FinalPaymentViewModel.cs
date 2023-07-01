f1beb75e31e4ba445b3961f18a2820848755c048
Wouter Altepost <wout.altepost@gmail.com>
2023-07-01 13:51:30 +0200

    changes

---
+++ GUI/ViewModels/FinalPaymentViewModel.cs
@@ -0,0 +1,39 @@
+using System.Reactive;
+using ExCSS;
+using GUI.ViewModels;
+using ModelLayer.Tables;
+using ReactiveUI;
+using ServiceLayer.Tables;
+
+namespace GUI.Views;
+
+public class FinalPaymentViewModel : RoutablePage
+{
+    public TablesService service = new();
+    public override IHostScreen HostScreen { get; }
+
+    public ReactiveCommand<Unit, Unit> GoBack { get; set; }
+    
+    public ReactiveCommand<Unit, Unit> Pay { get; set; }
+
+    public FinalPaymentViewModel(IHostScreen screen) 
+    {
+        HostScreen = screen;
+        
+        GoBack = ReactiveCommand.Create(() => // setting value
+        {
+            screen.GoBack();
+        });
+        
+        Pay = ReactiveCommand.Create(() =>
+        {
+            screen.GoNext(new TransactionPaymentViewModel(screen));
+            
+            // free ta le
+            service.Free(new Table()
+            {
+                ID = HostScreen.CurrentTable
+            });
+        });
+    }
+}
